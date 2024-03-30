585c3716bec9e506582e7fe73af6af92cb60ff20
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-03-30 12:28:27 +0700

    Codingan js

---
+++ script.js
@@ -0,0 +1,19 @@
+function showAlert(){
+    alert(Date(),tampil(),show(), hai())
+    Date()
+
+    let tanggal = new Date();
+    Document.getElementById("hasil").innerHTML - tanggal;
+
+    function tampil (){
+        alert("Burung kenari adalah burung kicau yang memiliki kicauan yang merdu")
+    }
+
+    function show(){
+        alert("Kenari merah memiliki banyak peminat karena memiliki warna yang unik")
+    }
+
+    function hai(){
+        alert("Kenari jambul memiliki jambul bagaikan burung yang menggunakan wig")
+    }
+}
